ac9de482a69410e3ea27dd25eedc1f07808bc137
Rushikeshavachat <119668833+Rushikeshavachat@users.noreply.github.com>
2024-02-17 11:38:40 +0530

    fix error

--- client/page.tsx
+++ client/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import SignIn from './app/login/page'
+// import SignIn from './app/login/page'
+import Form from './components/Form'
 
 const page = () => {
   return (
     <div>
-      <SignIn/>
+      {/* <SignIn/> */}
+      <Form/>
     </div>
   )
 }
